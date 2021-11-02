30d782f52bff40bb7e3e1abdf61409be457f475d
Antonio Ruben Paitno <antonio.patino003@gmail.com>
2021-11-02 15:36:50 -0500

    commit tree b7d012e333e3f13bbde6cabc70c9f91a61449176

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(Ride){
+  if(Ride <= 400)
+  return 'This one is on me!';
+
+  else if (Ride >=2000 && Ride < 2500)
+  return 'I will gladly take your thirty bucks.' ;
+
+  else if (Ride >=2500)
+  return 'No can do.'
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(NYC){
+  return NYC === 'NYC'? 'Ok, sounds good.' :'No go.'
 }
+ 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
+
+
+function switchOnCharmFromTip(Tip){
+ switch(Tip){
+   case 'generous':
+     return 'Thank you so much.'
+     case 'not as generous':
+       return 'Thank you.'
+       case 'thanks for everything':
+         return 'Bye.'
+       
+    
+ }
 }
